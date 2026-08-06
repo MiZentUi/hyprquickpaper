d582e84437923a5d415d23efbddf8a51a2b30fdc
29121970t <29121970tttt@gmail.com>
2026-08-06 21:22:53 +0300

    commands.sh cleanup
    	commands.sh
    
    	modified:   commands.sh

--- commands.sh
+++ commands.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+<<<<<<< HEAD
 
 # swww img $1 -t grow --transition-duration 1
 # wpaperctl set $1
+=======
+swww img $1 -t grow --transition-duration 1
+
+>>>>>>> 3617e2f (commands.sh cleanup)
